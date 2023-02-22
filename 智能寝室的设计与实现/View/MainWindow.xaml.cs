4cc20e4128ff5393f2086d367806d321a591d4a7
Administrator <Administrator@DESKTOP-CUG9PQA>
2023-02-22 15:08:58 +0800

    h

--- 智能寝室的设计与实现/View/MainWindow.xaml.cs
+++ 智能寝室的设计与实现/View/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -18,23 +19,54 @@ namespace 智能寝室的设计与实现.View
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
+   
     public partial class MainWindow : Window
     {
+       
         public MainWindow()
         {
             InitializeComponent();
 
-           
+            //popup1.VerticalOffset = 150;
+            //popup1.HorizontalOffset = 50;
+
+
+            //popup1.VerticalOffset = -60;
+            //popup1.HorizontalOffset = -95;
+            //kongjian.TranslatePoint(mousePoint,grid);
+            //
+            //
+
+
+        }
+        public CustomPopupPlacement[] placePopup(Size popupSize, Size targetSize, Point offset)
+        {
+            CustomPopupPlacement placement1 =  new CustomPopupPlacement(new Point(5,5), PopupPrimaryAxis.Vertical);
+             
+
+            CustomPopupPlacement placement2 = new CustomPopupPlacement(new Point(0, 0), PopupPrimaryAxis.Horizontal);
+               
+
+            CustomPopupPlacement[] ttplaces =      new CustomPopupPlacement[] { placement1, placement2 };
+              
+            return ttplaces;
+        }
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
         }
 
-        #region MyRegion
-        //protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
-        //{
-        //   if (e.LeftButton == MouseButtonState.Pressed)
-        //    {
-        //        this.DragMove();
-        //    }
-        //} 
-        #endregion
+        private void Border_MouseLeave(object sender, MouseEventArgs e)
+        {
+            //this.UpdateLayout();
+          //  Point point=    e.GetPosition(grid);
+            Point  mousePoinst = kongjian.TranslatePoint(new Point(0,0) ,grid);
+          
+            //   popup1.CustomPopupPlacementCallback = new CustomPopupPlacementCallback(placePopup);
+
+        }
     }
 }
